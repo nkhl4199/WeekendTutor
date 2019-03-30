2237ae254004afd3ee49960b84816f51b996e3d2
vsushruth <32487818+vsushruth@users.noreply.github.com>
2019-03-30 13:00:36 +0530

    Update students.java

--- app/src/main/java/com/chetan/wt/students.java
+++ app/src/main/java/com/chetan/wt/students.java
@@ -2,49 +2,66 @@ package com.chetan.wt;
 
 public class students {
 
+    String id;
     String mail;
     String qualification;
     String name;
     String city;
     int wallet;
+    String durl;
 
     public students() {
     }
 
 
-
-    public String getMail() {
-        return mail;
+    public students(String id, String name, String email, String degree, String city, String durl, int wallet) {
+        this.id = id;
+        this.name = name;
+        this.mail = email;
+        this.qualification = degree;
+        this.city = city;
+        this.durl = durl;
+        this.wallet = wallet;
     }
-    public void setMail(String mail) {
-        this.mail = mail;
+    public students(String id, String name, String email, String degree, String city, int wallet) {
+        this.id = id;
+        this.name = name;
+        this.mail = email;
+        this.qualification = degree;
+        this.city = city;
+        this.durl="https://i.imgur.com/tGbaZCY.jpg";
+        this.wallet = wallet;
+    }
+
+
+
+    public String getDurl() {
+        return durl;
     }
 
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
+
+    public String getMail() {
+        return mail;
     }
 
     public String getQualification() {
         return qualification;
     }
-    public void setQualification(String qualification) {
-        this.qualification = qualification;
+
+    public String getId() {
+        return id;
     }
 
     public String getCity() {
         return city;
     }
-    public void setCity(String city) {
-        this.city = city;
-    }
 
     public int getWallet() {
         return wallet;
     }
-    public void setWallet(int wallet) {
-        this.wallet = wallet;
-    }
 }
+
+
