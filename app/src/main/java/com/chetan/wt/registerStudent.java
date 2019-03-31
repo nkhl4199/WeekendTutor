f6e9b653af77ffa5188a9162ded051950f4b7ae7
vsushruth <32487818+vsushruth@users.noreply.github.com>
2019-03-31 23:37:00 +0530

    Update registerStudent.java

--- app/src/main/java/com/chetan/wt/registerStudent.java
+++ app/src/main/java/com/chetan/wt/registerStudent.java
@@ -2,6 +2,7 @@ package com.chetan.wt;
 
 import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -56,6 +57,7 @@ public class registerStudent extends AppCompatActivity {
     private FirebaseAuth fa;
     private DatabaseReference dataBase;
     private StorageReference storageReference;
+    SharedPreferences sp;
     //private FusedLocationProviderClient mFusedLocationClient;
 
     String downloadUrl = new String("https://i.imgur.com/tGbaZCY.jpg");
@@ -85,6 +87,7 @@ public class registerStudent extends AppCompatActivity {
         setContentView(R.layout.activity_register_student);
 
         Intent intent = getIntent();
+        sp = getSharedPreferences("login",MODE_PRIVATE);
 
         mAuth = FirebaseAuth.getInstance();
         fa = FirebaseAuth.getInstance();
@@ -205,6 +208,9 @@ public class registerStudent extends AppCompatActivity {
                                 dataBase.child(id).setValue(student_user);
 
                                 pb.dismiss();
+                                sp.edit().putString("userClass", "Student").apply();
+                                sp.edit().putBoolean("loginStatus", true).apply();
+                                
                                 Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_LONG).show();
                                 Newpage.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                                 Newpage.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
